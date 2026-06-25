1212b50b1ba7af24e9bec171b8184515e5b0fbba
Pragyan chaudhary <panwarpragyan01@gmail.com>
2026-06-25 20:34:47 +0530

    Fix duplicate Streamlit button IDs

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -67,9 +67,9 @@ def student_dashboard():
 
         stats = stats_map.get(sid,{"total":0, "attended": 0} )
         def unenroll_button():
-                if st.button("Unenroll from tihs course", type='tertiary', width='stretch', icon=':material/delete_forever:'):
+                if st.button("Unenroll from this course",key=f"unenroll_{sid}", type='tertiary', width='stretch', icon=':material/delete_forever:'):
                     unenroll_student_to_subject(student_id, sid)
-                    st.toast(f'Unenrolled from {sub['name']} successfully!')
+                    st.toast(f"Unenrolled from {sub['name']} successfully!")
                     st.rerun()
 
         with cols[i % 2]:
